cc0ba01e104027549f304e725140d353153b244d
aasuyadav1 <aasuy284@gmail.com>
2025-02-09 18:09:53 +0530

    fix: api route

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, login, getAllUsers, getRemainingUsersInOrganization } from '../controller/auth-controller.js';
+import { register, login, getAllUsers, getRemainingUsersInOrganization, updateProfile } from '../controller/auth-controller.js';
 import { authMiddleware } from '../middleware/auth-middleware.js';
 
 const router = Router();
@@ -7,6 +7,7 @@ const router = Router();
 router.post('/register', register);
 router.post('/login', login);
 router.get('/users', authMiddleware, getAllUsers);
+router.put('/profile', authMiddleware, updateProfile);
 router.get('/organizations/:orgId/remaining-users', authMiddleware, getRemainingUsersInOrganization);
 
 export const authRoutes = router;
